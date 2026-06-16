6d886ab5ba0f63963f1a2e56ac655a1441f09b1f
W-DevEndd <W-DevEndd@github.com>
2026-06-16 15:25:44 +0700

    add some tweak register

--- src/components/Tweak.ts
+++ src/components/Tweak.ts
@@ -1,12 +1,22 @@
+import type { BooleanProps } from "../types/properties/BooleanProps";
+import type { RangeProps } from "../types/properties/RangeProps";
 import type { TweakProps } from "../types/properties/TweakProps";
 import TweakTypes from "../types/TweakTypes";
 
 export class Tweak<T extends TweakProps> {
+
     private type: TweakTypes;
     private props: T;
 
-    constructor(type: TweakTypes, props: T) {
+    private constructor(type: TweakTypes, props: T) {
         this.type = type;
         this.props = props;
+    }
+
+    static bool(props: BooleanProps) {
+        return new Tweak<BooleanProps>(TweakTypes.Boolean, props);
+    }
+    static range(props: RangeProps) {
+        return new Tweak<RangeProps>(TweakTypes.Range, props);
     }
 }
--- src/types/properties/TweakProps.ts
+++ src/types/properties/TweakProps.ts
@@ -1,7 +1,4 @@
 import type { BooleanProps } from "./BooleanProps";
 import type { RangeProps } from "./RangeProps";
 
-export interface TweakProps {
-    Bool: BooleanProps;
-    Range: RangeProps;
-}
+export type TweakProps = BooleanProps | RangeProps;
