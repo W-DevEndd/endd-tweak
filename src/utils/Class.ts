38044ef5bad63a70de3d73b2ff3595c5b7fac7eb
W-DevEndd <W-DevEndd@github.com>
2026-06-17 12:48:59 +0700

    init

---
+++ src/utils/Class.ts
@@ -0,0 +1,12 @@
+
+export class Class {
+    private static bind()     {}
+    private static call()     {}
+    private static apply()    {}
+    private static toString() {}
+
+    private static name = "";
+    private static caller = "";
+    private static length = "";
+    private static arguments = "";
+}
